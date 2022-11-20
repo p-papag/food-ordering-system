b5fbaba2437a0dba5235b454c375b42a1b6b47f5
Panos Papagiannopoulos <thedrpapa@gmail.com>
2022-11-20 15:49:14 +0200

    First commit

---
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/output/repository/OrderRepository.java
@@ -0,0 +1,16 @@
+package com.food.ordering.system.order.service.domain.ports.output.repository;
+
+import com.food.ordering.system.domain.valueobject.OrderId;
+import com.food.ordering.system.order.service.domain.entity.Order;
+import com.food.ordering.system.order.service.domain.valueobject.TrackingId;
+
+import java.util.Optional;
+
+public interface OrderRepository {
+
+    Order save(Order order);
+
+    Optional<Order> findById(OrderId orderId);
+
+    Optional<Order> findByTrackingId(TrackingId trackingId);
+}
